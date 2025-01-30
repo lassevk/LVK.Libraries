04626ec6b6d0ab62af7a3da8171e5bb175d03f3a
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2025-01-30 12:26:19 +0100

    Structure

--- sandboxes/Sandbox.Console/Program.cs
+++ sandboxes/Sandbox.Console/Program.cs
@@ -8,4 +8,5 @@ HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 builder.Bootstrap(new ModuleBootstrapper());
 
 IHost host = builder.Build();
+await host.InitializeAsync();
 await host.RunAsConsoleApplicationAsync<Application>();
