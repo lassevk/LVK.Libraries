c943a384a0502d7acc1806b34cbf1b10fcc301fc
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2025-04-06 18:27:36 +0200

    Add service extension for event handling
    Introduce `AddEvents` extension method to simplify event handling registration through the DI container. This adds `IEvents` as a singleton service, improving integration with the service collection. Update naming rules in ReSharper settings for better adherence to style conventions.

---
+++ src/LasseVK.Events/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace LasseVK.Events;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddEvents(this IServiceCollection services)
+    {
+        services.AddSingleton<IEvents, Events>();
+
+        return services;
+    }
+}
