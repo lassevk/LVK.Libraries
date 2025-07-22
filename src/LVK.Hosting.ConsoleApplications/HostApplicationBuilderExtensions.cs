49a91956ce16baf7ffc80816e778a578935c7277
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2025-07-22 11:10:09 +0200

    Add `LVK.Hosting` project and migrate `HostApplicationBuilderExtensions`
    Introduced a new `LVK.Hosting` project with .NET 9.0 support. Migrated `HostApplicationBuilderExtensions` from `LVK.Hosting.ConsoleApplications` to `LVK.Hosting` for improved modularity. Updated references and project structure accordingly.

--- src/LVK.Hosting.ConsoleApplications/HostApplicationBuilderExtensions.cs
+++ src/LVK.Hosting.ConsoleApplications/HostApplicationBuilderExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System.Diagnostics;
 
+using JetBrains.Annotations;
+
 using LVK.Hosting.ConsoleApplications.Internal;
 
 using Microsoft.Extensions.Configuration;
@@ -9,40 +11,9 @@ using Microsoft.Extensions.Hosting;
 
 namespace LVK.Hosting.ConsoleApplications;
 
+[PublicAPI]
 public static class HostApplicationBuilderExtensions
 {
-    public static IHostApplicationBuilder AddStandardConfigurationSources<TProgram>(this IHostApplicationBuilder builder)
-        where TProgram : class
-    {
-        IConfigurationSource[] beforeJson = builder.Configuration.Sources.TakeWhile(source => source is not JsonConfigurationSource).ToArray();
-        IConfigurationSource[] afterJson = builder.Configuration.Sources.SkipWhile(source => source is not JsonConfigurationSource).SkipWhile(source => source is JsonConfigurationSource).ToArray();
-        builder.Configuration.Sources.Clear();
-        foreach (IConfigurationSource source in beforeJson)
-        {
-            builder.Configuration.Sources.Add(source);
-        }
-
-        string environmentName = builder.Environment.EnvironmentName;
-
-        builder.Configuration.SetBasePath(Path.GetDirectoryName(typeof(TProgram).Assembly.Location)!);
-        builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-        builder.Configuration.AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true);
-        builder.Configuration.AddJsonFile($"appsettings.{Environment.MachineName}.json", optional: true, reloadOnChange: true);
-        builder.Configuration.AddJsonFile($"appsettings.{Environment.MachineName}.{environmentName}.json", optional: true, reloadOnChange: true);
-
-        foreach (IConfigurationSource source in afterJson)
-        {
-            builder.Configuration.Sources.Add(source);
-        }
-
-        if (builder.Environment.IsDevelopment() || Debugger.IsAttached)
-        {
-            builder.Configuration.AddUserSecrets<TProgram>();
-        }
-
-        return builder;
-    }
-
     public static IHostApplicationBuilder AddConsoleApplication<T>(this IHostApplicationBuilder builder, Action<T>? configure = null)
         where T : class, IConsoleApplication
     {
