e948a9225e05090d5378e3839bc2ea6f0b6ad99f
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2025-12-04 00:45:54 +0100

    Add `IHostInitializer` and `HostExtensions` to support modular host initialization

---
+++ src/LVK.Bootstrapping/HostExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace LVK.Bootstrapping;
+
+public static class HostExtensions
+{
+    extension<T>(T host)
+        where T : IHost
+    {
+        public async Task Initialize()
+        {
+            var moduleInitializers = host.Services.GetServices<IModuleInitializer>().ToList();
+            foreach (IModuleInitializer initializer in moduleInitializers)
+            {
+                await initializer.InitializeAsync(CancellationToken.None);
+            }
+
+            var initializers = host.Services.GetServices<IHostInitializer<T>>().ToList();
+            foreach (IHostInitializer<T> initializer in initializers)
+            {
+                await initializer.InitializeAsync(host);
+            }
+        }
+    }
+}
