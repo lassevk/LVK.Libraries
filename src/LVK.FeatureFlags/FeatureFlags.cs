caff04be5a3d7d3d24bad9c6d048f6da979990ec
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2025-12-18 08:21:40 +0100

    Remove Flagsmith due to it being severely limited in free version.

--- src/LVK.FeatureFlags/FeatureFlags.cs
+++ src/LVK.FeatureFlags/FeatureFlags.cs
@@ -1,7 +1,5 @@
 using System.Text;
 
-using Flagsmith;
-
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 
@@ -11,28 +9,12 @@ internal class FeatureFlags : IFeatureFlags
 {
     private readonly IConfiguration _configuration;
     private readonly IOptions<FeatureFlagsOptions> _options;
-    private readonly FlagsmithClient? _flagsServerClient;
-
-    private IFlags? _environmentFlags;
 
     public FeatureFlags(IConfiguration configuration, IOptions<FeatureFlagsOptions> options)
     {
         _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
         _options = options ?? throw new ArgumentNullException(nameof(options));
-
-        if (_options.Value.EnvironmentKey != null)
-        {
-            _flagsServerClient = CreateFlagsmithClient();
-        }
     }
-
-    private FlagsmithClient CreateFlagsmithClient() => new(new FlagsmithConfiguration
-    {
-        EnvironmentKey = _options.Value.EnvironmentKey!,
-        ApiUri = new Uri(_options.Value.FlagsServerUrl),
-        EnableLocalEvaluation = true,
-        EnvironmentRefreshInterval = TimeSpan.FromSeconds(10),
-    });
 
     public async Task<bool> IsEnabled(string flagName)
     {
@@ -40,16 +22,6 @@ internal class FeatureFlags : IFeatureFlags
         if (isLocallyEnabled)
         {
             return true;
-        }
-
-        if (_flagsServerClient != null)
-        {
-            _environmentFlags ??= await _flagsServerClient.GetEnvironmentFlags();
-            IFlag? flag = await _environmentFlags.GetFlag(flagName);
-            if (flag != null)
-            {
-                return flag.Enabled;
-            }
         }
 
         return false;
--- src/LVK.FeatureFlags/FeatureFlagsOptions.cs
+++ src/LVK.FeatureFlags/FeatureFlagsOptions.cs
@@ -3,19 +3,10 @@ namespace LVK.FeatureFlags;
 public class FeatureFlagsOptions
 {
     public string SectionName { get; set; } = "FeatureFlags";
-    public string? EnvironmentKey { get; set; }
-    public string FlagsServerUrl { get; set; } = "https://flags.vkarlsen.no";
 
     public FeatureFlagsOptions UseSectionName(string sectionName)
     {
         SectionName = (sectionName ?? throw new ArgumentNullException(nameof(sectionName))).Trim();
-        return this;
-    }
-
-    public FeatureFlagsOptions UseFlagsServer(string environmentKey, string? flagsServerUrl = null)
-    {
-        EnvironmentKey = (environmentKey ?? throw new ArgumentNullException(nameof(environmentKey))).Trim();
-        FlagsServerUrl = flagsServerUrl ?? FlagsServerUrl;
         return this;
     }
 }
